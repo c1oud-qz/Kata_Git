b0a76253e27053069201a8d8fce9fb383517e90f
c1oud-qz <pangurbanq@gmail.com>
2023-07-24 00:10:30 +0300

    Bug fixes

--- Main.java
+++ Main.java
@@ -12,86 +12,110 @@ public class Main {
         if (text.length()<3){
             throw new IOException("Неправильный ввод.");
         }
+        System.out.println(calc(text));
+    }
+    public static String[] separateSymbols(String str_to_sep) throws IOException {
+        String[] separated = new String[3];
+        String first = "", second = "";
+        char op = 48;
+        int i = 0;
+        String operators = "*+-/";
+        boolean isNotOne = false;
+        for (int l=0; l<str_to_sep.length(); l++){
+            char elem = str_to_sep.charAt(l);
+            if ((operators.indexOf(elem)==-1)){
+                continue;
+            }
+            else{
+                isNotOne = true;
+            }
+        }
+        if (isNotOne){
+            do{
+                char temp = str_to_sep.charAt(i);
+                first += Character.toString(temp);
+                i++;
+                if ((str_to_sep.charAt(i)=='*')||(str_to_sep.charAt(i)=='+')||(str_to_sep.charAt(i)=='-')||(str_to_sep.charAt(i)=='/')){
+                    separated[0] = first;
+                    op = str_to_sep.charAt(i);
+                    first = Character.toString(op);
+                    separated[1] = first;
+                    i++;
+                }
+            }
+            while(op == 48);
+            do{
+                char temp = str_to_sep.charAt(i);
+                second += Character.toString(temp);
+                i++;
+            }
+            while((i<str_to_sep.length())&&(op != 48)&&(str_to_sep.charAt(i)>'/'));
+            separated[2] = second;
+            
+            if (i<str_to_sep.length()){
+                throw new IOException("Ввод более двух операндов!");
+            }
+            return separated;
+        }
+        else{
+            throw new IOException("Неправильный ввод.");
+        }
+    }
+    
+    public static String calc(String input) throws IOException{
+        String arr[] = separateSymbols(input);
+        char oper = arr[1].charAt(0);
         String rim [] = new String[]{"I", "V", "X", "L", "C"};
+        String str_match = "IVXLC";
+        for (int i=0; i<input.length(); i++){
+            char elem = input.charAt(i);
+            if (str_match.indexOf(elem)==-1){
+                if(Character.isDigit(elem)==false){
+                    if ((elem=='*')||(elem=='+')||(elem=='-')||(elem=='/')){
+                        continue;
+                    }
+                    else{
+                        throw new IOException("Неправильный ввод.");
+                    }
+                }
+            }
+        }
+
         boolean needRim = false;
-        for (int i=0; i<text.length(); i++){
+        for (int i=0; i<input.length(); i++){
             boolean isRim = false;
             for (int k=0; k<rim.length; k++){
-                if ((Character.toString(text.charAt(i))).equals(rim[k])) {
+                if ((Character.toString(input.charAt(i))).equals(rim[k])) {
                     isRim = true;
                     needRim = isRim;
                 }
             }
         }
-        for (int i=0; i<text.length(); i++){
-            if ((needRim==true)&&((Character.isDigit(text.charAt(i)))==true)){
+        for (int i=0; i<input.length(); i++){
+            if ((needRim==true)&&((Character.isDigit(input.charAt(i)))==true)){
                 throw new IOException("Несопоставимый ввод! Либо римские, либо арабские цифры в одном выражении.");
             }
             else {
                 continue;
             }
         }
-        
-        if (needRim == true){
-            String first = "", second = "";
-            char op = 48;
-            int i = 0;
-            do{
-                char temp = text.charAt(i);
-                first += Character.toString(temp);
-                i++;
-                if ((text.charAt(i)=='*')||(text.charAt(i)=='+')||(text.charAt(i)=='-')||(text.charAt(i)=='/')){
-                    op = text.charAt(i);
-                    i++;
-                }
-            }
-            while(op == 48);
-            do{
-                char temp = text.charAt(i);
-                second += Character.toString(temp);
-                i++;
-            }
-            while((i<text.length())&&(op != 48)&&(text.charAt(i)>'/'));
-            
-            /* new method convertRimToArab
-                value
-             */
-            int conv1 = convertRimToArab(first);
-            int conv2 = convertRimToArab(second);
-            if ((conv1<=conv2)&&((op == '-')||(op == '/'))){
+        if (needRim == false){
+            int a = Integer.valueOf(arr[0]);
+            int b = Integer.valueOf(arr[2]);
+            int str = doMath(a, oper, b);
+            return Integer.toString(str);
+        }
+        else{
+            int conv1 = convertRimToArab(arr[0]);
+            int conv2 = convertRimToArab(arr[2]);
+            if ((conv1<=conv2)&&((oper == '-')||(oper == '/'))){
                 throw new IOException("В римской системе счисления результат не может быть записан в виде отрицательного числа или 0.");
             }
-            // System.out.println(calc(conv1, op, conv2));
-            int conv_result = calc(conv1, op, conv2);
-            // new method convertArabToRim(conv_result)
-            System.out.println(convertArabToRim(conv_result));
-        }
-
-        else{
-            int first = 0, second = 0, temp = 0;
-            char op = 48;
-            for (int i=0; i<text.length(); i++){
-                if (Character.isDigit(text.charAt(i)) == true){
-                    temp = temp*10 + Character.getNumericValue(text.charAt(i));
-                    if (Character.toString(op)!="0"){
-                        second = temp;
-                    }
-                }
-                else{
-                    if ((text.charAt(i)=='+')||(text.charAt(i)=='-')||(text.charAt(i)=='/')||(text.charAt(i)=='*')){
-                        first = temp;
-                        temp = 0;
-                        op = text.charAt(i);
-                    }
-                    else {
-                        throw new IOException("Неправильный формат вводимого действия.");
-                    }
-                }
-            }
-            System.out.println(calc(first, op, second));
+            int conv_result = doMath(conv1, oper, conv2);
+            return convertArabToRim(conv_result);
         }
     }
-    public static int calc(int a, char oper, int b) throws IOException{
+    public static int doMath(int a, char oper, int b) throws IOException{
         if ((a>10)||(b>10)){
             throw new IOException("Вводимые числа не должны быть больше 10!");
         }
